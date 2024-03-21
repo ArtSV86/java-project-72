b029534a6c74eff011be417879432a5cfad312ea
Art <Art.S.V.86@yandex.ru>
2024-03-21 12:35:54 +0400

    Added files

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,80 @@
+package hexlet.code;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.ResourceCodeResolver;
+import hexlet.code.controller.RootController;
+import hexlet.code.controller.UrlCheckController;
+import hexlet.code.controller.UrlController;
+import hexlet.code.repository.BaseRepository;
+import hexlet.code.util.NamedRoutes;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
+
+
+
+@Slf4j
+public class App {
+
+    public static void main(String[] args) throws SQLException, IOException {
+        var app = getApp();
+        app.start(getPort());
+    }
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+    private static final String JDBC_URL_H2 = "jdbc:h2:mem:project";
+    static String jdbcUrlCurrent = getJdbcDatabaseUrl();
+    public static String getJdbcDatabaseUrl() {
+        return System.getenv().getOrDefault("JDBC_DATABASE_URL", JDBC_URL_H2);
+    }
+
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        return TemplateEngine.create(codeResolver, ContentType.Html);
+    }
+
+    public static Javalin getApp() throws SQLException {
+
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(jdbcUrlCurrent);
+
+        var dataSource = new HikariDataSource(hikariConfig);
+
+        var inputStream = App.class.getClassLoader().getResourceAsStream("schema.sql");
+        var reader = new BufferedReader(new InputStreamReader(inputStream));
+        var sql = reader.lines().collect(Collectors.joining("\n"));
+
+        log.info(sql);
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+        BaseRepository.dataSource = dataSource;
+
+        var app = Javalin.create(config -> config.plugins.enableDevLogging());
+
+        JavalinJte.init(createTemplateEngine());
+
+        app.get(NamedRoutes.mainPath(), RootController::index);
+        app.get(NamedRoutes.urlsPath(), UrlController::index);
+        app.get(NamedRoutes.urlPath("{id}"), UrlController::show);
+        app.post(NamedRoutes.urlsPath(), UrlController::create);
+        app.post(NamedRoutes.urlChecksPath("{id}"), UrlCheckController::create);
+
+        return app;
+    }
+}
